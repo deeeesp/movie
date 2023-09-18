4cbeec88b756e5d1a5e96be265f15f15467160b6
deeeesp <36041666+deeeesp@users.noreply.github.com>
2023-09-18 21:29:46 +0300

    Administrator data upd

--- src/main/resources/migrations/fill-db/fill_user_table.sql
+++ src/main/resources/migrations/fill-db/fill_user_table.sql
@@ -8,4 +8,4 @@ insert into app_user (status, email, password, role, username)
 values ('ACTIVE', 'liluser@mail.ru','qwerty1234', 'USER', 'liluser');
 
 insert into app_user (status, email, password, role, username)
-values ('ACTIVE', 'movieadmin@mail.ru','admin', 'ADMIN', 'admin');
+values ('ACTIVE', 'movieadmin@mail.ru','$2a$10$TCybbGW95XZh7tZpKBKwTeb0NXECk4iH1tD3Pz2PZnNMi51BuxAlu', 'ADMIN', 'administrator');
